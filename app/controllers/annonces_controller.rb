2a608fc81121078b7abf5adb1ba3d99e8c02ff52
Redfact <tantelyrandria@ymail.com>
2020-06-01 23:57:09 +0200

    add annonces show

--- app/controllers/annonces_controller.rb
+++ app/controllers/annonces_controller.rb
@@ -2,4 +2,8 @@ class AnnoncesController < ApplicationController
   def index
      @annonces = Annonce.all
   end
+
+  def show 
+    @annonce = Annonce.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'annonces#index'
   devise_for :users
 
+  resources :annonces
+
 end

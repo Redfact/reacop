656fce794e89a4ddf9b8eaaa8c0b9d52b52cab92
Redfact <tantelyrandria@ymail.com>
2020-06-25 00:34:36 +0200

    add methods for annonces and data getters

--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -10,4 +10,55 @@ class Annonce < ApplicationRecord
 
     has_many :photos
 
+
+    def self.locations
+        Annonce.all.select{ |an| an.location? == true}
+    end
+
+    def self.ventes
+        Annonce.all.select{ |an| an.vente? == true}
+    end
+    
+    def self.appartements
+        Annonce.all.select{ |an| an.appartement? == true}
+    end
+
+    def self.maisons
+        Annonce.all.select{ |an| an.maison? == true}
+    end
+
+    def self.priceDesc
+        Annonce.all.order(loyer_total: :desc)
+    end
+
+    def self.priceAsc
+        Annonce.all.order(loyer_total: :asc)
+    end
+
+    def self.DateDesc
+        Annonce.all.order(created_at: :desc)
+    end
+
+    def self.DateAsc
+        Annonce.all.order(created_at: :asc)
+    end
+
+    def to_s
+        "Lieu :#{self.lieu}
+        Capacite : #{self.capacite} chambres
+        Loyer total : #{self.loyer_total}
+        Description : #{self.description}
+        #{self.typeBien}
+        #{self.typeVente}"
+    end
+    
+    def self.search(input)
+        results=[]
+        Annonce.all.each do |an|
+            if( an.to_s.downcase.delete(' ').include?(input.downcase.delete(' ')))then
+                results<<an
+            end
+        end
+        results
+    end
 end

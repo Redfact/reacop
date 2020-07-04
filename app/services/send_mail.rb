959aec67207aeed96e0626c922331a64c2077df5
Redfact <tantelyrandria@ymail.com>
2020-07-04 17:28:33 +0200

    debug mailer

--- app/services/send_mail.rb
+++ app/services/send_mail.rb
@@ -4,15 +4,21 @@ include SendGrid
 class SendMail
 
     def initialize(nom,email,objet,message)
-    msg = "Message de"+message+"Mail:#{email}"
+    msg= 
+    "Message de #{nom.upcase} :
+            
+    #{message}
+          
+    Adresse email : #{email}"
+
     puts "Sending mail"
     puts nom,email,objet,msg
     from = SendGrid::Email.new(email: "tantelyrandria@ymail.com")
     # reagencym@gmail.com
-    to = SendGrid::Email.new(email:"reaimmo@yopmail.com")
-    subject =objet
+    to = SendGrid::Email.new(email:email)
+    subject =" #{objet}"
     content = SendGrid::Content.new(type: 'text/plain', 
-    value: msg.concat(email))
+    value: msg )
     mail = SendGrid::Mail.new(from, subject, to, content)
     
     sg = SendGrid::API.new(api_key: 'SG.QL_h5_QXRsScHG5pA0U3hA.hHy9uFS33du1M0zGakls55kRDQ0b5sAu8BMSr6cDA60')

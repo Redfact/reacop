902a0687e3d42d343377357738aa0c5a0e174e08
Redfact <tantelyrandria@ymail.com>
2020-07-06 21:53:54 +0200

    add form for new annonce

--- app/controllers/annonces_controller.rb
+++ app/controllers/annonces_controller.rb
@@ -22,9 +22,35 @@ class AnnoncesController < ApplicationController
     def show 
         @annonce = Annonce.find(params[:id])
         @place_libre= @annonce.place_libre
-        @place_occupe= @annonce.capacite - @place_libre
-        @partiel = @annonce.contrat_partiel
         @photos = @annonce.photos
+    end
+
+    def new
+        @annonce= Annonce.new(place_libre:0)
+        @error = params[:error]
+        if( @error != nil)
+            @annonce=Annonce.new(annonce_params)
+        end
+    end
+
+    def create
+        puts "Controller Annonce create",annonce_params
+        @annonce = Annonce.new(annonce_params)
+        puts @annonce
+        @annonce.save
+        if( @annonce.save)
+            redirect_to(root_path(:create_annonce=>true))
+        else
+            @error = @annonce.errors.full_messages
+            redirect_to new_annonce_path(:error=>@error ,:annonce => annonce_params)
+        end
+    end
+
+    def valide_params
+    end
+
+    def annonce_params
+        params.require(:annonce).permit(:loyer_total,:description,:lieu,:capacite,:typeVente,:typeBien,:AlaUne)
     end
   
 end
--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -10,6 +10,12 @@ class Annonce < ApplicationRecord
 
     has_many :photos
 
+    validates :lieu , presence: true
+    validates :typeBien , presence: true
+    validates :typeVente , presence: true
+    validates :description , presence:true , length: {minimum:5}
+    validates :capacite , presence: true ,  numericality: { only_integer: true , greater_than: 0  }
+
     def self.AlaUne
         Annonce.all.select{ |an| an.AlaUne == true}
     end
@@ -36,12 +42,13 @@ class Annonce < ApplicationRecord
     end
     
     def to_s
-        "Lieu :#{self.lieu}
-        Capacite : #{self.capacite} chambres
-        Loyer total : #{self.loyer_total}
-        Description : #{self.description}
-        #{self.typeBien}
-        #{self.typeVente}"
+"Lieu :#{self.lieu}
+Capacite : #{self.capacite} chambres
+Loyer total : #{self.loyer_total} Ar
+Description : #{self.description}
+Type de bien : #{self.typeBien}
+Type de vente : #{self.typeVente}
+A la une ? => #{self.AlaUne}"
     end
     
     def self.search(input)

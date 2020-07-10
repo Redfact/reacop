7ebd99ffed5d3c73f9aea39889fbac9513e6743e
Redfact <tantelyrandria@ymail.com>
2020-07-10 14:40:00 +0200

    add front forms

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,20 +2,27 @@ require 'send_mail'
 class ContactsController < ApplicationController
 
   def show
-    @commit=params[:commit]
-    if( @commit.eql?("Envoyer") )
-      @commit=true
-      @validate_mail=false
-      @email= params['/contactez_nous'][:email]
-      @nom=params['/contactez_nous'][:nom]
-      @objet=params['/contactez_nous'][:objet]
-      @message=params[:message]
-      puts "Values".colorize(:green) , @commit , @email, @nom,@objet,@message
-      if( !@email.empty? && !@nom.empty? && !@message.empty? )then
-        @validate=true
-        ContactMailer.transmit_email(@nom,@email,@message).deliver_now!
-      end
-    end
-  end
+      @commit=params[:commit]
+      if( @commit.eql?("Envoyer") )
+          @commit=true
+          @validate_mail=false
+          @email= params['/contactez_nous'][:email]
+          @nom=params['/contactez_nous'][:nom]
+          @objet=params['/contactez_nous'][:objet]
+          @message=params[:message]
+          puts "Values".colorize(:green) , @commit , @email, @nom,@objet,@message
 
+          if( !@email.empty? && !@nom.empty? && !@message.empty? )then
+            @validate=true
+            Message.create(
+              contenu:
+              "Nom :#{@nom}
+              Message:#{@message}
+              Mail client: #{@email}
+            ")
+            ContactMailer.transmit_email(@nom,@email,@message).deliver_now!
+            redirect_to root_path(:contact => true )
+          end
+      end
+  end
 end

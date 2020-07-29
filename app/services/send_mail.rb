74f92bfcced737d5937cb2d7b79932a0ab67108e
Redfact <tantelyrandria@ymail.com>
2020-07-29 22:42:34 +0200

    remove sendrid service

--- app/services/send_mail.rb
+++ app/services/send_mail.rb
@@ -1,6 +1,4 @@
 require 'mailjet'
-require 'sendgrid-ruby'
-include SendGrid
 
 class SendMail
     attr_accessor :nom , :email , :objet , :message
@@ -9,32 +7,6 @@ class SendMail
         @email=email
         @objet=objet
         @message=message
-    end
-
-    def sendgrid
-        msg= 
-"Message de #{@nom.upcase} :
-                
-#{@message}
-              
-Adresse email : #{@email}"
-    
-        puts "Sending mail"
-        puts @nom,@email,@objet,@msg
-        from = SendGrid::Email.new(email: "no_reply@reagency-mada.com")
-        #reagencym@gmail.com
-        to = SendGrid::Email.new(email:@email)
-        subject =" #{@objet}"
-        content = SendGrid::Content.new(type: 'text/plain', 
-        value: msg )
-        mail = SendGrid::Mail.new(from, subject, to, content)
-        
-        sg = SendGrid::API.new(api_key: 'SG.QL_h5_QXRsScHG5pA0U3hA.hHy9uFS33du1M0zGakls55kRDQ0b5sAu8BMSr6cDA60')
-        response = sg.client.mail._('send').post(request_body: mail.to_json)
-        puts response.status_code
-        puts response.body
-        puts response.headers
- 
     end
 
     def mailjet
